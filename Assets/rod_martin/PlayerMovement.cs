10d378d08124a3422c9b7e809dbe2c112de2741d
rod_martin <r.martin1@salford.ac.uk>
2021-11-10 11:01:02 +0000

    scriptable object demo

---
+++ Assets/rod_martin/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace rod_martin
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] public PlayerProps playerProps;
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.Translate(playerProps.speed, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.Translate(playerProps.speed, 0, 0);
+            }
+        }
+    }
+}
