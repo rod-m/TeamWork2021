b6d850bdbd44126f9b8a96206919b983cb99e986
rod_martin <r.martin1@salford.ac.uk>
2021-11-10 12:02:04 +0000

    sound demo start

---
+++ Assets/rod_martin/AudioEventBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace rod_martin
+{
+   
+    public abstract class  AudioEventBase : ScriptableObject
+    {
+        public AudioClip[] clips;
+        public abstract void Play(AudioSource _source);
+    }
+}
---
+++ Assets/rod_martin/SimpleSounds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace rod_martin
+{
+    [CreateAssetMenu(fileName = "simple_sound", menuName = "Add simple sound", order = 0)]
+    public class SimpleSounds : AudioEventBase
+    {
+        public override void Play(AudioSource _source)
+        {
+            if (clips.Length == 0) return;
+            _source.clip = clips[Random.Range(0, clips.Length)];
+            _source.volume = Random.Range(0, 1);
+            _source.pitch = Random.Range(0, 1);
+
+        }
+    }
+}
