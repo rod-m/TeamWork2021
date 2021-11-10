14aeb67b5d8ac0b552310815b3ee804a6574b525
Ritualos <K.Antzas@edu.salford.ac.uk>
2021-11-10 11:01:58 +0000

    this is Kon

---
+++ Assets/Kon_Antzas/PlayerProp.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Kon_Antzas
+{
+    [CreateAssetMenu(fileName = "PlayerProp", menuName = "Make Player Props", order = 0)]
+    public class PlayerProp : ScriptableObject
+    {
+        
+    }
+}
