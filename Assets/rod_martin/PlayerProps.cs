55ef4dda0c63e9118f10d09e9730b64ace4e492f
rodaraknia <rod@araknia.com>
2021-11-11 11:47:00 +0000

    cleanup removed files

--- Assets/rod_martin/PlayerProps.cs
+++ Assets/rod_martin/PlayerProps.cs
@@ -5,6 +5,6 @@ namespace rod_martin
     [CreateAssetMenu(fileName = "PlayerPropertiesRod", menuName = "Add Player Prop Rod", order = 0)]
     public class PlayerProps : ScriptableObject
     {
-        public float speed = 10f;
+        public float speed = 9f;
     }
 }
