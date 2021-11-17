72dbec102130029ee5dc62256d4f4d6ad351223e
rod_martin <r.martin1@salford.ac.uk>
2021-11-17 11:24:10 +0000

    Basic singleton pattern in Unity

---
+++ Assets/rod_martin/SingletonGameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace rod_martin
+{
+    public class SingletonGameManager : MonoBehaviour
+    {
+        public static SingletonGameManager Instance;
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+            }else{
+                Instance = this;
+            }
+        }
+
+    }
+}
